567044a8b262a4238756cefd61d395c8e0e6c732
ainesmile <ainesmile@gmail.com>
2017-04-10 21:13:26 +1200

    create add_tweet page

--- mysite/twitter/models.py
+++ mysite/twitter/models.py
@@ -1,5 +1,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Following(models.Model):
+    username = models.CharField(max_length=200)
+    joined_date = models.DateTimeField()
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ['joined_date']
+
+class Tweet(models.Model):
+    author = models.ForeignKey(Following)
+    context = models.TextField()
+    created_date = models.DateTimeField(
+        default = timezone.now
+    )
+
+    def __str__(self):
+        return self.author
+
+    class Meta:
+        ordering = ['-created_date']
+
+
--- mysite/twitter/views.py
+++ mysite/twitter/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
+from .models import Tweet
+from .forms import TweetForm
 
 def tweet_list(request):
     return render(request, 'twitter/tweet_list.html', {})
+
+def add_tweet(request):
+    # form = TweetForm(request.POST)
+    if request.method == 'POST':
+        form = TweetForm(request.POST)
+        if form.is_valid():
+            tweet = form.save()
+    else:
+        form = TweetForm()
+    return render(request, 'twitter/add_tweet.html', {
+        'form': form
+    })
