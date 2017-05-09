c322d9ea5ea2ae562622ae40824bf6bf6982adfd
ainesmile <ainesmile@gmail.com>
2017-05-09 22:25:38 +1200

    rewrite get_receivers to get a tweet's stream receivers

--- mysite/twitter/views/services/stream.py
+++ mysite/twitter/views/services/stream.py
@@ -20,32 +20,13 @@ def is_receiver(tweet, follower):
 
 def get_receivers(tweet):
     author = tweet.author
-    receivers = [author]
-    followships = Followship.objects.filter(followed_user=user)
+    receivers = set([author])
+    followships = Followship.objects.filter(followed_user=author)
     for followship in followships:
         follower = followship.initiative_user
         if is_receiver(tweet, follower):
-            receivers.append(follower)
-
-
-
-# def get_receivers(tweet, stream_type):
-#     user = tweet.author
-#     receivers = set([user])
-#     followships = Followship.objects.filter(followed_user=user)
-#     for followship in followships:
-#         follower = followship.initiative_user
-#         if stream_type == 'Y':
-#             replyships = Replyship.objects.filter(reply=tweet)
-#             if replyships:
-#                 reply_user = replyships[0].reply_user
-#                 be_replied_user = replyships[0].tweet_user
-
-#                 if be_replied_user == follower or check_followship(follower, be_replied_user):
-#                     receivers.add(follower)
-#         else:
-#             receivers.add(follower)
-#     return receivers
+            receivers.add(follower)
+    return receivers
 
 def create_streams(tweet, stream_type):
     receivers = get_receivers(tweet, stream_type)
