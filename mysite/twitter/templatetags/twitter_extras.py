bdb72f00f96b94433527cf3281d354c907c16b6e
ainesmile <ainesmile@gmail.com>
2017-05-15 14:25:27 +1200

    add except for display time

--- mysite/twitter/templatetags/twitter_extras.py
+++ mysite/twitter/templatetags/twitter_extras.py
@@ -6,21 +6,23 @@ register = template.Library()
 
 @register.filter(name='display_time', expects_localtime=True)
 def display_time(value):
-    
-    delta = timezone.now() - value
-    delta_seconds = delta.total_seconds()
-    seconds_per_hour = 3600
-    seconds_per_min = 60
-    
-    if delta >= timedelta(hours=24):
-        return value.strftime('%b %d')
-    elif delta > timedelta(hours=1):
-        delta_hours = int(delta_seconds / seconds_per_hour)
-        return ("%dhour" %delta_hours if delta_hours == 1 else "%dhours" %delta_hours)
-    elif delta >= timedelta(minutes=1):
-        delta_mins = int(delta_seconds / seconds_per_min)
-        return ("%dmin" %delta_mins if delta_mins == 1 else "%dmins" %delta_mins)
-    elif delta >= timedelta(seconds=1):
-        return "%ds" %int(delta_seconds)
-    else:
-        return 'now'
+    try:
+        delta = timezone.now() - value
+        delta_seconds = delta.total_seconds()
+        seconds_per_hour = 3600
+        seconds_per_min = 60
+        
+        if delta >= timedelta(hours=24):
+            return value.strftime('%b %d')
+        elif delta > timedelta(hours=1):
+            delta_hours = int(delta_seconds / seconds_per_hour)
+            return ("%dhour" %delta_hours if delta_hours == 1 else "%dhours" %delta_hours)
+        elif delta >= timedelta(minutes=1):
+            delta_mins = int(delta_seconds / seconds_per_min)
+            return ("%dmin" %delta_mins if delta_mins == 1 else "%dmins" %delta_mins)
+        elif delta >= timedelta(seconds=1):
+            return "%ds" %int(delta_seconds)
+        else:
+            return 'now'
+    except TypeError:
+        return value
